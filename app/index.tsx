f5bfffdac60036ed22521061c15d6b1fdaf59aa9
khoidm2004 <khoidm2004@gmail.com>
2025-05-06 17:00:57 +0300

    Finised set up tailwind

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,12 @@
 import { Text, View } from "react-native";
+import "./global.css";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Farting bird is here</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-5xl text-accent font-bold">
+        Farting bird is here
+      </Text>
     </View>
   );
 }
