77a927aff6061af91ef90edaea0134965ab4889a
khoidm2004 <khoidm2004@gmail.com>
2025-05-12 02:18:52 +0300

    Upload source code

---
+++ services/appwrite.ts
@@ -0,0 +1,51 @@
+import { Client, Databases, ID, Query } from "appwrite";
+
+const DB_ID = process.env.EXPO_PUBLIC_APPWRITE_DB_ID!;
+const COLLECTION_ID = process.env.EXPO_PUBLIC_APPWRITE_COLLECTION_ID!;
+
+const client = new Client()
+  .setEndpoint("https://fra.cloud.appwrite.io/v1")
+  .setProject(process.env.EXPO_PUBLIC_APP_ID!);
+
+const database = new Databases(client);
+
+export const updateSearchCount = async (query: string, movie: Movie) => {
+  try {
+    const result = await database.listDocuments(DB_ID, COLLECTION_ID, [
+      Query.equal("searchTerm", query),
+    ]);
+
+    if (result.documents.length > 0) {
+      const existingMovie = result.documents[0];
+      await database.updateDocument(DB_ID, COLLECTION_ID, existingMovie.$id, {
+        count: existingMovie.count + 1,
+      });
+    } else {
+      await database.createDocument(DB_ID, COLLECTION_ID, ID.unique(), {
+        searchTerm: query,
+        movie_id: movie.id,
+        count: 1,
+        title: movie.title,
+        poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+      });
+    }
+  } catch (error) {
+    console.error("Error in updateSearchCount:", error);
+    throw error;
+  }
+};
+
+export const getTrendingMovies = async (): Promise<
+  TrendingMovie[] | undefined
+> => {
+  try {
+    const result = await database.listDocuments(DB_ID, COLLECTION_ID, [
+      Query.limit(5),
+      Query.orderDesc("count"),
+    ]);
+
+    return result.documents as unknown as TrendingMovie[];
+  } catch (error) {
+    return undefined;
+  }
+};
